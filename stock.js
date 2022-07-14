a93b62cea03e84113872b802adaf2156697665fc
Alvarez Axel <gabriel8037.ga@gmail.com>
2022-07-14 17:25:54 -0300

    proyecto final javascript

---
+++ stock.js
@@ -0,0 +1,6 @@
+let stockProductos = [
+    {id: 1, nombre: "producto 1", tipo: "producto", cantidad: 1, desc: "Tóner de refinación de poros Neutrogena con hamamelis", precio: 949, img: './img/porerefining.jpg'},
+    {id: 2, nombre: "producto 2", tipo: "producto", cantidad: 1, desc: "Neutrogena Hydro Boost Gel limpiador suave", precio: 1053, img: './img/hydroboost.jpg'},
+    {id: 3, nombre: "producto 3", tipo: "producto", cantidad: 1, desc: "Máscara facial de avena Aveeno con extracto de semilla de calabaza calmante", precio: 1376, img: './img/mascaraaveno.jpg'},
+    {id: 4, nombre: "producto 4", tipo: "producto", cantidad: 1, desc: "L'Oréal Paris Derm Intensives Vitamin C Serum", precio: 2987, img: './img/lorealvitaminaC.jpg'},
+]
